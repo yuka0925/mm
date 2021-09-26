a7e4cdf37922f9a3dbc4c1d4107b6a1e278a7ba0
yuka0925 <91425063+yuka0925@users.noreply.github.com>
2021-09-27 01:33:27 +0900

    Add files via upload

---
+++ m5-1.php
@@ -0,0 +1,96 @@
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <lang="ja">
+    <title></title>
+  </head>
+  <body>
+    </body>
+</html>
+<?php
+//データベース接続
+    $dsn = 'データベース名';
+    $user = 'ユーザー名';
+    $password = 'パスワード';
+    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));$id = 2;
+    //データベース内にテーブルを作成
+    $sql = "CREATE TABLE IF NOT EXISTS tbtest"
+    ." ("
+    . "id INT AUTO_INCREMENT PRIMARY KEY,"
+    . "name char(32),"
+    . "comment TEXT"
+    .");";
+    $stmt = $pdo->query($sql);
+    //データベースのテーブル一覧を表示
+    $sql ='SHOW TABLES';
+    $result = $pdo -> query($sql);
+    foreach ($result as $row){
+        echo $row[0];
+        echo '<br>';
+    }
+    echo "<hr>";
+    //テーブルの詳細確認
+    $sql ='SHOW CREATE TABLE tbtest';
+    $result = $pdo -> query($sql);
+    foreach ($result as $row){
+        echo $row[1];
+    }
+    echo "<hr>";
+    //データ入力
+　　$sql = $pdo -> prepare("INSERT INTO tbtest (name, comment) VALUES (:name, :comment)");
+    $sql -> bindParam(':name', $name, PDO::PARAM_STR);
+    $sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
+    $name = '（好きな名前）';
+    $comment = '（好きなコメント）'; //好きな名前、好きな言葉は自分で決めること
+    $sql -> execute();
+//データレコードを抽出し、表示
+    $sql = 'SELECT * FROM tbtest';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+    foreach ($results as $row){
+        //$rowの中にはテーブルのカラム名が入る
+        echo $row['id'].',';
+        echo $row['name'].',';
+        echo $row['comment'].'<br>';
+    echo "<hr>";
+    }
+//データレコード内容編集
+ 　$id = 1; //変更する投稿番号
+    $name = "（変更したい名前）";
+    $comment = "（変更したいコメント）"; //変更したい名前、変更したいコメントは自分で決めること
+    $sql = 'UPDATE tbtest SET name=:name,comment=:comment WHERE id=:id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+//表示
+$sql = 'SELECT * FROM tbtest';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+    foreach ($results as $row){
+        //$rowの中にはテーブルのカラム名が入る
+        echo $row['id'].',';
+        echo $row['name'].',';
+        echo $row['comment'].'<br>';
+    echo "<hr>";
+    }
+//データレコードを削除
+$id = 2;
+    $sql = 'delete from tbtest where id=:id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    //表示
+    $sql = 'SELECT * FROM tbtest';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+    foreach ($results as $row){
+        //$rowの中にはテーブルのカラム名が入る
+        echo $row['id'].',';
+        echo $row['name'].',';
+        echo $row['comment'].'<br>';
+    echo "<hr>";
+    }
+?>
